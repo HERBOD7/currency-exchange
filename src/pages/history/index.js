51b3636af54e1ab92dcade23761e4f9de27a902c
HERBOD7 <herbodabdoli@gmail.com>
2022-10-20 23:36:38 +0330

    feat: add conversion history page

--- src/pages/history/index.js
+++ src/pages/history/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import ConversionTable from './components/ConversionTable';
+import './History.scss';
 
-const History = () => {
-  return <div>history</div>;
+const History = (props) => {
+  return (
+    <div>
+      <h2 className="font-page-title color-default">Conversion history</h2>
+      <ConversionTable />
+    </div>
+  );
 };
 
 export default History;
