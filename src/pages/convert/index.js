0b04b7f3940dc0a2b0ca3a9d3b9cfc3bc24926d3
HERBOD7 <herbodabdoli@gmail.com>
2022-10-21 12:35:33 +0330

    feat: add search params to the convert page

--- src/pages/convert/index.js
+++ src/pages/convert/index.js
@@ -1,4 +1,5 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import storage from '../../helper/storage';
 import ExchangeForm from './components/ExchangeForm';
 import HistoryTable from './components/HistoryTable';
@@ -14,6 +15,10 @@ const Convert = (props) => {
   const [rates, setRates] = useState();
   const [startDate, setStartDate] = useState();
   const [endDate, setEndDate] = useState();
+  // const [fromParam, setFromParam] = useState();
+  // const [toParam, setToParam] = useState();
+  // const [amountParam, setAmountParam] = useState();
+  const [searchParams] = useSearchParams();
 
   const storeExchangeHistory = (from, to, amount) => {
     const exchange = [
@@ -34,27 +39,6 @@ const Convert = (props) => {
   };
 
   //TODO: form validation
-  const fetchExchange = (amount, from, to) => {
-    fetch(
-      `https://api.exchangerate.host/convert?from=${from}&to=${to}&amount=${amount}`,
-      {
-        method: 'GET',
-      }
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const formattedResult = new Intl.NumberFormat('en-IN').format(
-          data.result
-        );
-        const rate = data.info.rate;
-        const queryInfo = data.query;
-        setResultValue(formattedResult);
-        setRateValue(rate);
-        setQuery(queryInfo);
-        storeExchangeHistory(queryInfo.from, queryInfo.to, queryInfo.amount);
-      });
-  };
-
   const fetchExchangeHistory = (start, end, from, to) => {
     fetch(
       `https://api.exchangerate.host/timeseries?start_date=${start}&end_date=${end}&base=${from}&symbols=${to}`,
@@ -68,10 +52,37 @@ const Convert = (props) => {
       });
   };
 
-  const convertCurrency = (amount, from, to) => {
-    fetchExchange(amount, from, to);
-    fetchExchangeHistory(startDate, endDate, from, to);
-  };
+  const convertCurrency = useCallback(
+    (amount, from, to) => {
+      const fetchExchange = (amount, from, to) => {
+        fetch(
+          `https://api.exchangerate.host/convert?from=${from}&to=${to}&amount=${amount}`,
+          {
+            method: 'GET',
+          }
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            const formattedResult = new Intl.NumberFormat('en-IN').format(
+              data.result
+            );
+            const rate = data.info.rate;
+            const queryInfo = data.query;
+            setResultValue(formattedResult);
+            setRateValue(rate);
+            setQuery(queryInfo);
+            storeExchangeHistory(
+              queryInfo.from,
+              queryInfo.to,
+              queryInfo.amount
+            );
+          });
+      };
+      fetchExchange(amount, from, to);
+      fetchExchangeHistory(startDate, endDate, from, to);
+    },
+    [endDate, startDate]
+  );
 
   const changeDuration = useCallback(
     (start, end) => {
@@ -84,11 +95,23 @@ const Convert = (props) => {
     [query]
   );
 
+  useEffect(() => {
+    if (searchParams.get('amount') && startDate && endDate) {
+      const amount = searchParams.get('amount');
+      const from = searchParams.get('from');
+      const to = searchParams.get('to');
+      // setFromParam(from);
+      // setToParam(to);
+      // setAmountParam(amount);
+      convertCurrency(amount, from, to);
+    }
+  }, [searchParams, startDate, endDate, convertCurrency]);
+
   return (
     <div>
       <h2 className="font-page-title color-default">I want to convert</h2>
       <ExchangeForm submitForm={convertCurrency} />
-      {resultValue && (
+      {resultValue ? (
         <ExchangeResult
           amount={query.amount}
           fromCurrency={query.from}
@@ -96,6 +119,8 @@ const Convert = (props) => {
           result={resultValue}
           rate={rateValue}
         />
+      ) : (
+        <div className="Convert__empty-result"></div>
       )}
       <hr className="mt-10" />
       <div className="mt-5">
